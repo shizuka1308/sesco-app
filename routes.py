2b61cca44d04adab071fca7595896733c0245213
shizuka1308 <kajol1308@gmail.com>
2023-09-20 22:42:10 -0400

    define route for retrieving reactors on outage by date range

--- routes.py
+++ routes.py
@@ -1,3 +1,14 @@
+# API endpoint to filter reactors on outage by date range
+@routes.route('/reactors/on_outage/filter', methods=['GET'])
+def filter_reactors_on_outage_by_date_route():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    if not (is_valid_date_format(start_date) and is_valid_date_format(end_date)):
+        return jsonify(INVALID_DATE_FORMAT), 400
+
+    reactors_on_outage = list_reactors_on_outage_query(start_date, end_date)
+    return jsonify(reactors_on_outage)
+    
 # API endpoint to get the last known outage date of a reactor
 @routes.route('/reactors/last_known_outage', methods=['GET'])
 def get_last_known_outage_date_for_reactor_route():
