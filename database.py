3a9069d767fa52f41db21afb2191c47c65cc010c
shizuka1308 <kajol1308@gmail.com>
2023-09-20 22:53:12 -0400

    create functions for retrieving reactors

---
+++ database.py
@@ -0,0 +1,35 @@
+from clickhouse_driver import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def create_clickhouse_client():
+    clickhouse_host = os.getenv('CLICKHOUSE_HOST')  
+    clickhouse_database = os.getenv('CLICKHOUSE_DATABASE')
+
+    return Client(host=clickhouse_host, database=clickhouse_database)
+
+# Function to retrieve all reactors with an optional filter by state
+def get_all_reactors(state_filter=None):
+    client = create_clickhouse_client()
+    query = f"SELECT * FROM reactors"
+    if state_filter:
+        query += f" WHERE State ILIKE '%{state_filter}%'"
+    result = client.execute(query)
+    client.disconnect()
+    reactors_list = []
+    for row in result:
+        reactor_dict = {
+            "YearOfUpdate": row[0],
+            "PlantNameUnitNumber": row[1],
+            "NRCReactorUnitWebPage": row[2],
+            "DocketNumber": row[3],
+            "LicenseNumber": row[4],
+            "Location": row[5], 
+            "NRCRegion": row[6],  
+            "State": row[7],  
+        }
+        reactors_list.append(reactor_dict)
+
+    return reactors_list
