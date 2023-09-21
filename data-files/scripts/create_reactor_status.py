ec2843479b864bef398290e10f54ad824223bb5f
shizuka1308 <kajol1308@gmail.com>
2023-09-20 22:37:37 -0400

    Create the reactor_status table

--- data-files/scripts/create_reactor_status.py
+++ data-files/scripts/create_reactor_status.py
@@ -13,6 +13,20 @@ def establish_clickhouse_connection():
     clickhouse_database = os.getenv("CLICKHOUSE_DATABASE")
     return Client(host=clickhouse_host, database=clickhouse_database)
 
+def create_reactor_status_table(client):
+    """Create the reactor_status table in ClickHouse if it doesn't exist."""
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS reactor_status
+    (
+        report_date Date,
+        reactor_name String,
+        power Int32
+    )
+    ENGINE = MergeTree()
+    ORDER BY report_date;
+    """
+    client.execute(create_table_query)
+    
 def main():
     try:
         client = establish_clickhouse_connection()
